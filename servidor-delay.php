2a50b93017f3525ca7135c60e3ce9e7232b9ca07
Vinicius Louzada <vinelouzada@gmail.com>
2023-07-13 17:18:15 -0300

    Incrementando no servidor-http

---
+++ servidor-delay.php
@@ -0,0 +1,4 @@
+<?php
+
+sleep(1);
+echo 'Resposta do servidor com delay de 3 segundos';
--- servidor-http.php
+++ servidor-http.php
@@ -7,7 +7,23 @@ use Swoole\Http\Server;
 $servidor = new Server('localhost','8080');
 
 $servidor->on('request', function (Request $requesicao, Response $resposta){
-   $resposta->end('gg');
+    $resultado = [];
+    Co::join([
+       go(function () use (&$resultado){
+           $cliente = new \Swoole\Coroutine\Http\Client('localhost',8081);
+           $cliente->get('/servidor-delay.php');
+           $conteudo = $cliente->getBody();
+
+           $resultado['servidor-delay'] = $conteudo;
+       }),
+       go(function () use (&$resultado){
+           $conteudo = file_get_contents('registros.txt');
+           $resultado['registros'] = $conteudo;
+       })
+    ]);
+
+    $resposta->end(json_encode($resultado));
+
 });
 
 $servidor->start();
